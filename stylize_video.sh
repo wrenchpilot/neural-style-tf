037b48c6e2bd067fec5f68df5d34a2a5264e58f7
wrenchpilot <wrenchpilot@gmail.com>
2018-07-12 22:03:01 -0400

    added image format and optimizer args

--- stylize_video.sh
+++ stylize_video.sh
@@ -13,25 +13,10 @@ command -v $FFMPEG >/dev/null 2>&1 || {
 }
 
 if [ "$#" -le 1 ]; then
-   echo "Usage: bash stylize_video.sh <path_to_video> <path_to_style_image>"
+   echo "Usage: bash stylize_video.sh <path_to_video> <path_to_style_image> <image_format (ppm,jpg,png)> <optimizer (lbfgs,adam)"
    exit 1
 fi
 
-echo ""
-read -p "Did you install the required dependencies? [y/n] $cr > " dependencies
-
-if [ "$dependencies" != "y" ]; then
-  echo "Error: Requires dependencies: tensorflow, opencv2 (python), scipy"
-  exit 1;
-fi
-
-echo ""
-read -p "Do you have a CUDA enabled GPU? [y/n] $cr > " cuda
-
-if [ "$cuda" != "y" ]; then
-  echo "Error: GPU required to render videos in a feasible amount of time."
-  exit 1;
-fi
 
 # Parse arguments
 content_video="$1"
@@ -45,6 +30,14 @@ style_image="$2"
 style_dir=$(dirname "$style_image")
 style_filename=$(basename "$style_image")
 
+image_format="$3"
+
+if [ "$4" ]; then
+  optimizer="$4"
+else
+  optimizer="lbfgs"
+fi
+
 if [ ! -d "./video_input" ]; then
   mkdir -p ./video_input
 fi
@@ -54,7 +47,7 @@ temp_dir="./video_input/${content_filename}"
 mkdir -p "$temp_dir"
 
 # Save frames of the video as individual image files
-$FFMPEG -v quiet -i "$1" "${temp_dir}/frame_%04d.ppm"
+$FFMPEG -v quiet -i "$1" "${temp_dir}/frame_%04d.${image_format}"
 eval $(ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=width,height "$1")
 width="${streams_stream_0_width}"
 height="${streams_stream_0_height}"
@@ -63,25 +56,26 @@ if [ "$width" -gt "$height" ]; then
 else
   max_size="$height"
 fi
-num_frames=$(find "$temp_dir" -iname "*.ppm" | wc -l)
+num_frames=$(find "$temp_dir" -iname "*.${image_format}" | wc -l)
 
 echo "Computing optical flow [CPU]. This will take a while..."
 cd ./video_input
-bash make-opt-flow.sh ${content_filename}/frame_%04d.ppm ${content_filename}
+bash make-opt-flow.sh ${content_filename}/frame_%04d.${image_format} ${content_filename}
 cd ..
 
 echo "Rendering stylized video frames [CPU & GPU]. This will take a while..."
-python neural_style.py --video \
+python3 neural_style.py --video \
 --video_input_dir "${temp_dir}" \
 --style_imgs_dir "${style_dir}" \
 --style_imgs "${style_filename}" \
 --end_frame "${num_frames}" \
 --max_size "${max_size}" \
+--optimizer "${optimizer}" \
 --verbose;
 
 # Create video from output images.
 echo "Converting image sequence to video.  This should be quick..."
-$FFMPEG -v quiet -i ./video_output/frame_%04d.ppm ./video_output/${content_filename}-stylized.$extension
+$FFMPEG -v quiet -i ./video_output/frame_%04d.${image_format} ./video_output/${content_filename}-stylized.$extension
 
 # Clean up garbage
 if [ -d "${temp_dir}" ]; then
